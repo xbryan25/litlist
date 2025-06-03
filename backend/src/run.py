4cbd29f779ea4b5dd0c4a676366d75fea5a42fa9
Bryan <bryanagan25@gmail.com>
2025-06-03 16:05:40 +0800

    feat(frontend, backend): configure run.py and connect backend to frontend via "/api/books"

---
+++ backend/src/run.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/books", methods=['GET'])
+def books():
+    value = {
+        "books": [
+            {
+                "id": '1',
+                "title": 'Harry Potter',
+                "genre": 'Fantasy',
+                "author": 'J.K. Rowling',
+                "pages": 350,
+                "readStatus": True
+            },
+            {
+                "id": '2',
+                "title": 'Lord of the Rings',
+                "genre": 'Adventure',
+                "author": 'Tolkien',
+                "pages": 670,
+                "readStatus": True
+            }
+        ]
+    }
+
+    return jsonify(value)
+
+
+def main():
+    app.run(debug=True, port=8080)
+
+
+if __name__ == "__main__":
+    main()
