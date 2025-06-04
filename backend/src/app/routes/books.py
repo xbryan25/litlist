c7b8334542f45bdea6347ef3656b551e42c218f4
Bryan <bryanagan25@gmail.com>
2025-06-04 10:06:46 +0800

    feat(frontend/components/form, backend/routes): make a POST request to the backend for add-book

--- backend/src/app/routes/books.py
+++ backend/src/app/routes/books.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from app.controllers import get_books_handler
 
 bp = Blueprint('books', __name__)
@@ -7,3 +7,12 @@ bp = Blueprint('books', __name__)
 @bp.route("", methods=['GET'])
 def get_books():
     return get_books_handler()
+
+
+@bp.route("/add-book", methods=['POST'])
+def add_book():
+    data = request.get_json()
+
+    print(data)
+
+    return jsonify({"message": "Book added successfully"}), 201
